f96bc3552f8d973d1b2d446d1c51804420f00b57
Fercho Alv <feral1592@gmail.com>
2024-09-10 23:18:18 -0600

    Update get_image_number result type

--- src/feature/manga_strategy/manga_scrapper_context.py
+++ src/feature/manga_strategy/manga_scrapper_context.py
@@ -25,16 +25,17 @@ class MangaScraper:
         self._logger.info("Trying to get page [%s: %s]",
                            image_name, image_number)
         folder.download_image(image_url, image_name, headers)
-        if current_page.is_last_page():
-          self._logger.info(
-            "Download of [%s] complete", current_page.get_manga_name())
-          break
-        current_page = current_page.get_next_page_async()
+
       except Exception as ex:
         errors.append(current_page.get_image_name())
         self._logger.error(
           "Page: %s, Error= %r",
           current_page.get_image_number(), ex, exc_info=True)
-        current_page = current_page.get_next_page_async()
 
+      if current_page.is_last_page():
+        break
+      current_page = current_page.get_next_page_async()
+
+    self._logger.info(
+      "Download of [%s] complete", current_page.get_manga_name())
     return errors
