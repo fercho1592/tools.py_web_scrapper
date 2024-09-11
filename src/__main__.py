7d8ccb98d71ef74b6dcfe26cccf887aa9e714545
Fercho Alv <feral1592@gmail.com>
2024-09-10 18:27:12 -0600

    set main file

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,11 @@
 '''Main code'''
 from infrastructure.file_downloader import FileDownloader
 from infrastructure.file_manager import read_queue
+from infrastructure.my_logger import get_logger
 from feature.manga_strategy.manga_factory import MangaFactory
 from feature.manga_strategy.manga_scrapper_context import MangaScraper
+
+_logger = get_logger(__name__)
 
 def main():
   download_queue = read_queue()
@@ -12,16 +15,20 @@ def main():
   for item in download_queue:
     print("*************************************************")
     strategy = MangaFactory.get_manga_strategy(item[0])
+    scrapper = MangaScraper(strategy)
+
     folder_manager = FileDownloader(f"../{item[1]}")
     folder_manager.create_folder_if_not_exist()
-    
-    scrapper = MangaScraper(strategy)
+
+    _logger.info("Start manga download for [%s]", item[1])
     error_by_manga = scrapper.run_manga_download_async(folder_manager, item[2])
 
     error_list[item[1]] = error_by_manga
 
     if len(error_by_manga) > 0:
       print(f"Error in these images from [{item[0]}]", error_by_manga[1])
+
+    _logger.info("End manga download for [%s]", item[1])
 
   print(error_list)
 
