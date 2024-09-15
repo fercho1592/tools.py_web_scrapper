bed98d71e66ba9cc954d68b74fbb6da599acc1fc
Fercho Alv <feral1592@gmail.com>
2024-09-14 21:17:46 -0600

    Add ingore case in read_queue folder

--- src/infrastructure/file_manager.py
+++ src/infrastructure/file_manager.py
@@ -4,6 +4,8 @@ def read_queue() -> list[list[str, str, str]]:
   tuplas = []
   with open("download-queue.txt", "r", encoding="utf-8") as archivo:
     for linea in archivo:
+      if linea.startswith("#") or linea.rstrip() == "":
+        continue
       elementos:list[str] = linea.strip().split("|")
 
       tupla = (
@@ -13,3 +15,4 @@ def read_queue() -> list[list[str, str, str]]:
         )
       tuplas.append(tupla)
   return tuplas
+
