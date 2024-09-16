755cccfaadb3a5b38d327042085634c2de76e366
Fercho Alv <feral1592@gmail.com>
2024-09-16 12:26:35 -0600

    rename _folder property

--- src/infrastructure/file_downloader.py
+++ src/infrastructure/file_downloader.py
@@ -8,18 +8,18 @@ import infrastructure.my_logger as my_logger
 class FileDownloader:
   '''Class to save images from http uri'''
   def __init__(self, folder: str):
-    self._folder = folder
+    self.folder_path = folder
     self.__logger = my_logger.get_logger(__name__)
 
   def create_folder_if_not_exist(self):
-    if not os.path.exists(self._folder):
-      self.__logger.debug("Creating [%s]", self._folder)
-      os.makedirs(self._folder)
+    if not os.path.exists(self. folder_path):
+      self.__logger.debug("Creating [%s]", self. folder_path)
+      os.makedirs(self. folder_path)
       return
-    self.__logger.debug("Folder [%s] exist", self._folder)
+    self.__logger.debug("Folder [%s] exist", self. folder_path)
 
   def download_image(self, url, image_name, headers):
-    path = f'{self._folder}/{image_name}'
+    path = f'{self. folder_path}/{image_name}'
     if os.path.exists(path):
       self.__logger.info("Duplicated image [%s]", image_name)
       return
@@ -29,5 +29,5 @@ class FileDownloader:
     self.__logger.info("Get image [%s]", image_name)
 
   def get_images_in_folder(self) -> list[str]:
-    elementos = os.listdir(self._folder)
+    elementos = os.listdir(self. folder_path)
     return elementos
