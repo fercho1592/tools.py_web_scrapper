1d9f85599de79f718892279f7d6cc328ad54557d
Fercho Alv <feral1592@gmail.com>
2024-09-16 12:49:46 -0600

    Update container

--- src/feature/manga_strategy/manga_implementations/container.py
+++ src/feature/manga_strategy/manga_implementations/container.py
@@ -1,7 +1,7 @@
 '''Set up all manga implementations'''
 from feature.manga_strategy.manga_interfaces import IMangaStrategy
 from feature.manga_strategy.manga_implementations.tmh import tmh_strategy
-from .e_web import e_web_strategy
+from feature.manga_strategy.manga_implementations.e_web import e_web_strategy
 
 MANGA_IMPLEMENTATIONS:list[IMangaStrategy] = [
   e_web_strategy.EMangaStrategy,
