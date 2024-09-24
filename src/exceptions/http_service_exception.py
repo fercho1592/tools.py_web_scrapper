8c95fc2c97496c88c8650ae4f52abc044d75d88f
Fernando Alvarez <fernando.alvarez@wizeline.com>
2024-09-23 20:57:28 -0600

    Add identation for Exception package

--- src/exceptions/http_service_exception.py
+++ src/exceptions/http_service_exception.py
@@ -1,5 +1,5 @@
 '''Exception for http services'''
 
 class HttpServiceException(Exception):
-  def __init__(self, message) -> None:
-    super().__init__(message)
+    def __init__(self, message) -> None:
+        super().__init__(message)
--- src/exceptions/manga_scrapper_exception.py
+++ src/exceptions/manga_scrapper_exception.py
@@ -1,7 +1,9 @@
+'''Exception for MangaScrapper'''
 class MangaScrapperException(Exception):
-  def __init__(self, url:str, page:int | None, max_page:int | None  ) -> None:
-    is_index_page = page is None
-    page_message = f"page ({page}/{max_page})"
-    index_message = "index page"
-    message = f"Error downloading [{url}] on {index_message if is_index_page else page_message}"
-    super().__init__(message)
+    '''Custom Exception for manga scraper'''
+    def __init__(self, url:str, page:int | None, max_page:int | None  ) -> None:
+        is_index_page = page is None
+        page_message = f"page ({page}/{max_page})"
+        index_message = "index page"
+        message = f"Error downloading [{url}] on {index_message if is_index_page else page_message}"
+        super().__init__(message)
