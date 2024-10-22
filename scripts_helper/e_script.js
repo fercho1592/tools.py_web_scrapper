2d9240af2a0a0856effd53e675df0587e4e18293
Fercho Alv <feral1592@gmail.com>
2024-10-22 00:12:16 -0600

    Update script

--- scripts_helper/e_script.js
+++ scripts_helper/e_script.js
@@ -1,5 +1,25 @@
 ((atistName)=>{
-    const rows = document.getElementsByClassName("gl3c glname")
+    const getTagsByTagType = (ele, tagType, ignore = "original")=>{
+        var rows =ele.getElementsByTagName("tr")
+    
+        const arrayEle = []
+        for (let index = 0; index < rows.length; index++) {
+            if(rows[index].childNodes[0].innerHTML == tagType)
+                arrayEle.push(rows[index]);
+        }
+
+        const tagList = arrayEle[0]?.querySelectorAll(".gt,.gtl") ?? []
+    
+        const tags = []
+        for (let index = 0; index < tagList.length; index++) {
+            if(tagList[index].innerHTML !== ignore)
+                tags.push(tagList[index].innerHTML);
+        }
+
+        return tags
+    }
+
+    const rows = document.getElementsByClassName("itg glte")[0].getElementsByClassName("gl4e glname")
 
     const arrayEle = []
     for (let index = 0; index < rows.length; index++) {
@@ -7,13 +27,21 @@
     }
 
     const result = arrayEle.map((element)=>{
-        const anchor = element.getElementsByTagName("a")[0]
-        const href = anchor.getAttribute("href")
+        const anchor = element.parentElement
+        const href = anchor.getAttribute("href")        
+        
+        const parody = getTagsByTagType(element, "parody:")
+
         const nameElement = anchor.getElementsByClassName("glink")[0]
         const regex = /\([^)]*\)|\[[^\]]*\]/gm;
-        let mangaName = nameElement.innerHTML.replace(regex,"").trim();
+        const mangaName = nameElement.innerHTML.replace(regex,"").trim();
+        let name = ""
+        if(parody[0] === undefined)
+            name = "[" + atistName + "]/" + mangaName;
+        else
+            name  = "[" + atistName + "]/[" + parody[0] + "]/" + mangaName;
 
-        const name = "[" + atistName + "]/" + mangaName;
+        name = name.toUpperCase().replace("|", "-").replace("~", "-").replace("?","").replace("!","")
 
         return (href + " | " + name).replace("\n", "")
     })
