fa84379d2bbe084bf02cefab8a2582cdca15ed5b
Fernando Alvarez <feral1592@gmail.com>
2025-11-07 14:30:13 -0600

    Add telegram bot (tests)

---
+++ src/telegram_bot.py
@@ -0,0 +1,39 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler,ContextTypes, Application
+from telegram.constants import ReactionEmoji
+from asyncio import sleep
+
+def BuildApp() -> Application:
+    import json
+
+    with open('env.json') as f:
+        env = json.load(f)
+
+    bot_token = env['telegram']['bot_token']
+
+    app = ApplicationBuilder().token(bot_token).build()
+    return app
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Hello! I am your bot.')
+
+async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    print(update.effective_user)
+    print(update.message.text)
+    await context.bot.set_message_reaction(chat_id=update.effective_chat.id,
+                                           message_id=update.message.message_id,
+                                           reaction=ReactionEmoji.GHOST)
+    await update.message.reply_text('No respondo pendejadas')
+    await sleep(2)
+    await update.message.reply_text('Y menos a maricas')
+
+if __name__ == '__main__':
+    print('Starting bot...')
+    app = BuildApp()
+
+    print('Registering handlers...')
+    app.add_handler(CommandHandler('start', start))
+    app.add_handler(MessageHandler(None, messageHandler))
+
+    print('Running bot...')
+    app.run_polling(allowed_updates=Update.ALL_TYPES)
