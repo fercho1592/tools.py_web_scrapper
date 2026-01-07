dc462ac37a74d989a17a714659da7e01edf5d317
Fernando Alvarez <feral1592@gmail.com>
2026-01-07 14:57:59 -0600

    add error handling to pepare links process

--- src/prepare_links.py
+++ src/prepare_links.py
@@ -1,7 +1,9 @@
+import re
 from configs import dependency_injection as IOT
 from configs.queue_reader import QueueItem
 from feature_interfaces.protocols.config_protocol import LoggerProtocol
 from feature.manga_strategy.manga_scrapper_context import MangaScraper
+
 
 def main() -> None:
     # initialize container and services
@@ -14,43 +16,75 @@ def main() -> None:
 
     # for each link
     for link in links:
-        scrapper:MangaScraper = container.resolve_factory(MangaScraper, link)
-        # get all metadata
-        manga_data = scrapper.get_manga_data()
-        # identify if is part of a series
-        if is_series(manga_data):
-            # identify chapter number
-            manga_data["chapter_number"] = identify_chapter_number(manga_data)
-        # Create queue item
+        try:
+            if [item for item in queue_list if item.MangaUrl == link]:
+                logger.info(f"Skipping duplicate link: {link}")
+                continue
+            logger.info(f"Processing link: {link}")
+            scrapper: MangaScraper = container.resolve_factory(MangaScraper, link)
+            # get all metadata
+            manga_data = scrapper.get_manga_data()
+            # identify if is part of a series
+            if is_series(manga_data):
+                # identify chapter number
+                manga_data["chapter_number"] = identify_chapter_number(manga_data)
+            # Create queue item
 
-        item = QueueItem(
-            manga_url=link,
-            path=create_path(manga_data),
-            page_number=0,
-            pdf_only=False,
-        )
-        # add to result list
-        queue_list.append(item)
+            item = QueueItem(
+                manga_url=link,
+                path=create_path(manga_data),
+                page_number=0,
+                pdf_only=False,
+            )
+            # add to result list
+            queue_list.append(item)
+        except Exception as e:
+            logger.error(f"Error processing link {link}: {e}")
+            continue
 
-    queue_list = sorted(queue_list, key=lambda x: x.path)
+    queue_list = sorted(queue_list, key=lambda x: x.FolderName)
 
     # serialize result list to file
     with open("temp-download-queue.txt", "w") as f:
         for item in queue_list:
-            f.write(f"{item.MangaUrl}|{item.FolderName}|{item.PageNumber}|{item.PdfOnly}\n")
+            f.write(
+                f"{item.MangaUrl}|{item.FolderName}|{item.PageNumber}|{item.PdfOnly}\n"
+            )
     pass
 
+
 def create_path(manga_data: dict) -> str:
-    pdf_name = manga_data.get("title", "manga") + ".pdf"
-    return str(pdf_name).upper()
+    regex = re.compile(r"\([^)]*\)|\[[^\]]*\]")
+    pdf_name = regex.sub("", manga_data.get("name")).strip() + ".pdf"
+    # get artist
+    artist = manga_data.get("artist", None)
+    # get groups
+    groups = manga_data.get("groups", "None")
+    artist = artist if artist else groups
+    result_path = f"[{artist}]/{pdf_name}"
+    result_path = (
+        str(result_path)
+        .replace("//", "-")
+        .replace("\\", "-")
+        .replace("|", "-")
+        .replace("\n", "")
+        .replace("~", "-")
+        .replace("?", "")
+        .replace("!", "")
+    )
+
+    return str(result_path).upper()
+
 
 def is_series(manga_data: dict) -> bool:
     # logic to determine if manga is part of a series
     return False
 
+
 def identify_chapter_number(manga_data: dict) -> str:
     # logic to identify chapter number from manga data
     return "1"
+
 
 def read_links_from_file(file_path: str = "links.txt") -> list[str]:
     links = []
@@ -60,6 +94,7 @@ def read_links_from_file(file_path: str = "links.txt") -> list[str]:
             if line and not line.startswith("#"):
                 links.append(line.split(" ")[-1])
     return links
+
 
 if __name__ == "__main__":
     main()
