fcbcde56b5644bfaa6bcfa89c45f2423a6f59b7a
Fernando Alvarez <fernando.alvarez@lexitaslegal.com>
2026-01-06 18:43:46 -0600

    remove not used method

--- src/configs/config_manager.py
+++ src/configs/config_manager.py
@@ -2,28 +2,6 @@
 import configparser
 
 @DeprecationWarning
-def read_config():
-    # Create a ConfigParser object
-    config = configparser.ConfigParser()
-
-    # Read the configuration file
-    config.read("./config.ini")
-
-    # Access values from the configuration file
-    log_level = config.get("General", "log_level")
-    e_manga_strategy = config.get("MangaStrategy", "e_manga_domain")
-    tmh_manga_domain = config.get("MangaStrategy", "tmh_manga_domain")
-
-    # Return a dictionary with the retrieved values
-    config_values = {
-      "log_level": log_level,
-      "e_manga_domain": e_manga_strategy,
-      "tmh_manga_domain": tmh_manga_domain,
-    }
-
-    return config_values
-
-@DeprecationWarning
 def read_azure_service_bus_config():
     config = configparser.ConfigParser()
     config.read("./config.ini")
